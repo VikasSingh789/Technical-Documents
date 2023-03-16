3c526fd8bc64df88bcedf53f9e41b5d9bd176b26
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2023-03-16 17:23:01 +0530

    Update SmallestAndPositionFromArray.java

--- Programs/UseCases/SmallestAndPositionFromArray.java
+++ Programs/UseCases/SmallestAndPositionFromArray.java
@@ -18,7 +18,25 @@ public class SmallestAndPositionFromArray {
 				position = i+1;
 			}
 		}
-		System.out.println("Second largest Number "+secmin+" Position is "+position);
+		
+		System.out.println("second smallest Number:- "+secmin+" found in position:-"+position);
+		
+		int max = arr[0];
+		int secmax = arr[1];
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]>max){
+				max = arr[i];
+			}
+		}
+		
+		int secMaxPosition = 0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>=secmax && arr[i]!=max) {
+				secmax = arr[i];
+				secMaxPosition=i+1;
+			}
+		}
+		System.out.println("second largest Number:- "+secmax+" found in position:-"+secMaxPosition);
 		
         }
 
