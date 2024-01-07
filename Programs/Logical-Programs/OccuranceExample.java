f65c8d0e084454929a72aa52924e2148084632d8
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-07 21:34:46 +0530

    Update OccuranceExample.java

--- Programs/Logical-Programs/OccuranceExample.java
+++ Programs/Logical-Programs/OccuranceExample.java
@@ -8,21 +8,23 @@ public class OccuranceExample {
 		char ch[] = str.toCharArray();
 		int i;
 		int count = 1;
-		for(i=0;i<ch.length-1;i++) {
-					if(ch[i]==ch[i+1]) {
-						
-						count = count+1;
-					}else {
-						sb.append(ch[i]);
-						sb.append(count);
-						count = 1;
-					}
-			
+		for (i = 0; i < ch.length - 1; i++) {
+
+			if (ch[i] == ch[i + 1]) {
+
+				count = count + 1;
+			} else {
+				sb.append(ch[i]);
+				sb.append(count);
+				count = 1;
+			}
+
 		}
 		sb.append(ch[i]);
 		sb.append(count);
 		System.out.println(sb);
-		
 	}
-
 }
+
+//input:-  aabacdd
+//output:- a2b1a1c1d2
