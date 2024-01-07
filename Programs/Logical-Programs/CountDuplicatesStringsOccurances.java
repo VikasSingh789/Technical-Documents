a6211c2bcde8b43cb4e233e8919cd9e217a4308b
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-07 20:53:56 +0530

    Update CountDuplicatesStringsOccurances.java

--- Programs/Logical-Programs/CountDuplicatesStringsOccurances.java
+++ Programs/Logical-Programs/CountDuplicatesStringsOccurances.java
@@ -9,29 +9,27 @@ public class CountDuplicatesStringsOccurances {
 		String strArr[] = str.split(" ");
 		TreeMap<String, Integer> map = new TreeMap<String, Integer>();
 		for (String string : strArr) {
-			if(map.containsKey(string)) {
-				map.put(string, map.get(string)+1);
-			}
-			else {
+			if (map.containsKey(string)) {
+				map.put(string, map.get(string) + 1);
+			} else {
 				map.put(string, 1);
 			}
 		}
 		System.out.println(map);
-		for (Map.Entry<String,Integer> string1 : map.entrySet()) {
-			if(string1.getValue()==1) {
-				//System.out.print(string1.getKey()+" contains "+string1.getValue()+" times" );
-				System.out.print(string1.getKey()+" " );
-			} 		
+		for (Map.Entry<String, Integer> word : map.entrySet()) {
+			if (word.getValue() > 1) {
+				System.out.println(word.getKey() + " contains " + word.getValue() + " times");
 			}
-		
-		
+		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		String str = "I am doing java java";
 		duplicateString(str);
 
 	}
-
 }
+
+//Input:-  I am doing java java
+//Output:- java contains 2 times
