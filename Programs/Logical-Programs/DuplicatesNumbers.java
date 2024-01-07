f8e5cde5bfcc1c1ce5fca13afa47222c2f8234d3
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-07 21:17:21 +0530

    Update DuplicatesNumbers.java

--- Programs/Logical-Programs/DuplicatesNumbers.java
+++ Programs/Logical-Programs/DuplicatesNumbers.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class DuplicatesNumbers {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Scanner scan=new Scanner(System.in);
-		int[] a={1,2,3,3,5};
-		for(int i=0;i<a.length;i++){
+	static void checkDuplicatedNumbers(int n[]) {
+		for (int i = 0; i < n.length; i++) {
 			int count = 1;
-			if(a[i]!=0){
-			for(int j=i+1;j<a.length;j++){
-				if(a[i] == a[j]){
-					a[j]=0;
-					count++;
+			if (n[i] != 0) {
+				for (int j = i + 1; j < n.length; j++) {
+					if (n[i] == n[j]) {
+						n[j] = 0;
+						count++;
+					}
 				}
-			}
-			if(count>1)
-				System.out.println(a[i]+" Repeated for"+count);
+				if (count > 1)
+					System.out.println(n[i] + " Repeated for " + count + " times");
 			}
 		}
 	}
+	
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 3, 5 };
+		AIPN.checkDuplicatedNumbers(arr);
 
+	}
 }
+
+//input:-   { 1, 2, 3, 3, 5, 5 };
+//output:-  3 Repeated for 2 times
