1e86729e6d2609d6c7e673479764e98b1f046b89
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-07 21:06:41 +0530

    Update DuplicateFirstCharacter.java

--- Programs/Logical-Programs/DuplicateFirstCharacter.java
+++ Programs/Logical-Programs/DuplicateFirstCharacter.java
@@ -3,38 +3,42 @@ package simpleprograms;
 import java.util.Scanner;
 
 public class DuplicateFirstCharacter {
+
 	static String checkDuplicateFirstCharacter(String str) {
 		String res = "";
-		try{
+		try {
 			char ch[] = str.toCharArray();
-			for(int i=0;i<ch.length;i++) {
+			for (int i = 0; i < ch.length; i++) {
 				int count = 1;
-				if(ch[i]!= ' ') {
-					for(int j=i+1;j<ch.length;j++) {
-						if(ch[i]==ch[j]) {
+				if (ch[i] != ' ') {
+					for (int j = i + 1; j < ch.length; j++) {
+						if (ch[i] == ch[j]) {
 							ch[j] = ' ';
 							count++;
 						}
-					}	
-					if(count>1) {
-						res=res+ch[i];
+					}
+					if (count > 1) {
+						res = res + ch[i];
 					}
 				}
 			}
-			if(res=="") {
+			if (res == "") {
 				System.out.println("No Duplicates found");
-			}else
-			res=res.substring(0,1).toUpperCase();
-		}
-		catch(Exception e) {
+			} else
+				res = res.substring(0, 1).toUpperCase();
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return res;
-		}
+	}
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the String");
-		String str=scan.nextLine();
-		System.out.println(checkDuplicateFirstCharacter(str));
+		System.out.println(checkDuplicateFirstCharacter("vikassi"));
 	}
 }
+
+//input:- vikass
+//output:- S (Capital S)
+
+//input:- vikassi
+//output:- I (Capital I. I because we found i as 1st duplicate character in given string)
