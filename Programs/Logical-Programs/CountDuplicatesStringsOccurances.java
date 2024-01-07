ad3ed78243bdc65f5934bfd60a2a74686b0c7b93
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-07 20:57:58 +0530

    Update CountDuplicatesStringsOccurances.java

--- Programs/Logical-Programs/CountDuplicatesStringsOccurances.java
+++ Programs/Logical-Programs/CountDuplicatesStringsOccurances.java
@@ -22,11 +22,27 @@ public class CountDuplicatesStringsOccurances {
 			}
 		}
 	}
+	
+	public static void duplicateStringWithoutCollection(String str) {
+		String strArr[] = str.split(" ");
+		for(int i=0;i<strArr.length;i++) {
+			int count = 1;
+			for(int j=i+1;j<strArr.length;j++) {
+				if(strArr[i].equals(strArr[j])) {
+					count = count+1;
+				}
+			}
+			if(count>1) {
+				System.out.println(strArr[i]+ " contains " + count + " times");
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 
 		String str = "I am doing java java";
 		duplicateString(str);
+		duplicateStringWithoutCollection(str);
 
 	}
 }
