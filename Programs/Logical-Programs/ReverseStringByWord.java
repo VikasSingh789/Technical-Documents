ace5978fd19359c8affba516238c76dfb0637907
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 12:04:44 +0530

    Update ReverseStringByWord.java

--- Programs/Logical-Programs/ReverseStringByWord.java
+++ Programs/Logical-Programs/ReverseStringByWord.java
@@ -1,26 +1,40 @@
 package com.programs;
 
 public class ReverseStringByWord {
-	
-			//Vikas Singh
-			//sakiV hgniS
-			//Singh Vikas
-			//hgniS sakiV
-	
-	static void swapString(String rev) {
-		
+	static void revStringByWord1(String rev) {
+
 		String arr[] = rev.split(" ");
 		String swap = "";
-		for(int i=arr.length-1;i>=0;i--) {
-		swap = swap+arr[i]+" ";	
+		for (int i = arr.length - 1; i >= 0; i--) {
+			swap = swap + arr[i] + " ";
 		}
 		System.out.println(swap.trim());
 	}
 
-	public static void main(String[] args) {
-		
-		StringBuffer sb = new StringBuffer("Vikas Singh");
-		swapString(sb.reverse().toString());
+	static void revStringByWord2(String str) {
+		String words[] = str.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			String rev = "";
+			for (int j = words[i].length() - 1; j >= 0; j--) {
+				rev += words[i].charAt(j);
+			}
+			System.out.print(rev + " ");
+		}
 	}
 
+	public static void main(String[] args) {
+
+		StringBuffer sb = new StringBuffer("Vikas Singh");
+		revStringByWord1(sb.reverse().toString());
+		System.out.println("==================");
+		String str = "Vikas Singh";
+		revStringByWord2(str);
+
+	}
 }
+
+//output:-
+//Vikas Singh
+//sakiV hgniS
+//Singh Vikas
+//hgniS sakiV
