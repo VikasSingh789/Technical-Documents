c9dbead5a7459f8bc0539d76f32121bec2388db1
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 13:33:02 +0530

    Update palindrome.java

--- Programs/Logical-Programs/palindrome.java
+++ Programs/Logical-Programs/palindrome.java
@@ -1,22 +1,30 @@
 package simple;
 import java.util.Scanner;
 public class palindrome {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int temp,rem,sum=0;
-		System.out.println("enter the number");
-		int n=scan.nextInt();
-		temp=n;
-		while(n>0)
-		{
-			rem=n%10;
-			sum=(sum*10)+rem;
-			n=n/10;
-		}
-	if(temp==sum)
-		System.out.println("palindrome");
-	else
-		System.out.println("not a palindrome");
-	}
-
+	
+	static boolean checkBoolean(int n) {
+        int temp = n;
+        int rev=0;
+        while(n>0){
+             int rem = n%10;
+             rev = rev*10+rem;
+             n = n/10;
+        }
+        if(temp==rev) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+ 
+    public static void main(String[] args) {
+        System.out.println(checkBoolean(234));
+    }
+ 
 }
+
+//input:-  242
+//output:- true
+
+//input:-  234
+//output:- false;
