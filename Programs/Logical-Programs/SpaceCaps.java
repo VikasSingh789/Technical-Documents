13dda2e45d20e60165dae98575c482537f89d5c7
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 12:22:27 +0530

    Update SpaceCaps.java

--- Programs/Logical-Programs/SpaceCaps.java
+++ Programs/Logical-Programs/SpaceCaps.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 
 public class SpaceCaps {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the String");
-		String str = scan.nextLine();
+	static void spaceCaps(String str) {
 		str = str.replaceAll("[^a-zA-Z0-9]", ":");
 		String[] res = str.split(":");
 		String output = "";
-		for(int i=0;i<res.length;i++) {
-		output = output+res[i].substring(0,1).toUpperCase()+res[i].substring(1);
+		for (int i = 0; i < res.length; i++) {
+			output = output + res[i].substring(0, 1).toUpperCase() + res[i].substring(1);
 		}
 		System.out.println(output);
 	}
 
+	public static void main(String[] args) {
+		String str = "Vikas singh";
+		spaceCaps(str);
+
+	}
 }
+
+//input:-   Vikas singh
+//output:-  VikasSingh
