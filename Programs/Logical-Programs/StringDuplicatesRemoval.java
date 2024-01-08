bb68151e515944f6605adc504b5ad92a1cab94b6
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 12:31:20 +0530

    Update StringDuplicatesRemoval.java

--- Programs/Logical-Programs/StringDuplicatesRemoval.java
+++ Programs/Logical-Programs/StringDuplicatesRemoval.java
@@ -3,27 +3,30 @@ package simpleprograms;
 import java.util.Scanner;
 
 public class StringDuplicatesRemoval {
-public static void main(String args[]){
-	Scanner scan=new Scanner (System.in);
-	System.out.println("enter the String");
-	String str= scan.nextLine();
-	//System.out.print("Duplicates Characters are:-");
-	System.out.print("String after remove duplicates:-");
-	char c[]=str.toCharArray();
-	for(int i=0;i<c.length;i++){
-		int count = 1;
-		if(c[i]!='~'){
-			for(int j=i+1;j<c.length;j++){
-				if(c[i] == c[j]){
-					c[j]='~';
-					count++;
+	static void removeDuplicatesFromString(String str) {
+		char c[]=str.toCharArray();
+		for(int i=0;i<c.length;i++){
+			int count = 1;
+			if(c[i]!='~'){
+				for(int j=i+1;j<c.length;j++){
+					if(c[i] == c[j]){
+						c[j]='~';
+						count=count+1;
+					}
 				}
 			}
-//			if(count>1)
-//				System.out.print(c[i]);
+			if(c[i]!='~')
+				System.out.print(c[i]);
 		}
-		if(c[i]!='~')
-			System.out.print(c[i]);
 	}
-}
-}
+
+	public static void main(String args[]){
+		String str = "vikass";
+		System.out.println("Given String:- "+str);
+		System.out.print("String after remove duplicates:- ");
+		removeDuplicatesFromString(str);
+	}
+	}
+
+//input:-  vikass
+//output:- vikas
