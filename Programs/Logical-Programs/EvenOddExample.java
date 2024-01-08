3eb61ffe28504cde287aaf5e057994e65b34d42b
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 20:09:28 +0530

    Create EvenOddExample.java

---
+++ Programs/Logical-Programs/EvenOddExample.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Programs {
+	public static void main(String[] args) {
+		List<Integer> listOfNumbers = Arrays.asList(1,2,3,4,5,6,7,8);
+		
+		Map<Boolean, List<Integer>> partitions = listOfNumbers.stream().collect(Collectors.partitioningBy(n->n%2==0));
+		System.out.println("By Using Partitions");
+		System.out.println(partitions.get(true));
+		System.out.println(partitions.get(false));
+		System.out.println("======================");
+		System.out.println("By Using grouping");
+		Map<Object, List<Integer>> groupingBy = listOfNumbers.stream().collect(Collectors.groupingBy(n -> n % 2 == 0 ? "EVEN" : "ODD"));
+		System.out.println(groupingBy.get("EVEN"));
+		System.out.println(groupingBy.get("ODD"));
+	}
+}
