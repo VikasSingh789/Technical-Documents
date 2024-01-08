8fa51cebdc576bf7b19153f585fb7ad9fb2ad3ec
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 13:47:39 +0530

    Update star.java

--- Programs/Logical-Programs/star.java
+++ Programs/Logical-Programs/star.java
@@ -1,23 +1,21 @@
 package simple;
 import java.util.Scanner;
-public class star {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		/*int count=0;
-		System.out.println("enter the number");
-		int n=scan.nextInt();
-		for(int i=0;i<n;i++){
-			System.out.print(++count+"*");
-			i=i*1
-	}*/
-			System.out.println("===================");
-			System.out.println("enter the number");
-			int n=4;
-			for(int i=1;i<=n;i++){
-				System.out.println(" ");
-				for(int j=1;j<=i;j++){
-				System.out.print("*"+i);
+public class SwapString {
+	static void swapString(String a, String b) {
+		System.out.println("Before Swap: a= " + a + ", b= " + b);
+		a = a.concat("0" + b);
+		b = a.split("0")[0];
+		a = a.split("0")[1];
+		System.out.println("After Swap: a= " + a + ", b= " + b);
 	}
-			}
+
+	public static void main(String args[]) {
+		String a = "Vikas";
+		String b = "Singh";
+		swapString(a, b);
 	}
 }
+
+
+//input:-  a=Vikas, b=Singh
+//output:- a=Singh, b=Vikas
