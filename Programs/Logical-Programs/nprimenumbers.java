39c24e49bac5815be1b817923dde4432d8557884
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-08 14:00:16 +0530

    Update nprimenumbers.java

--- Programs/Logical-Programs/nprimenumbers.java
+++ Programs/Logical-Programs/nprimenumbers.java
@@ -2,18 +2,28 @@ package simple;
 import java.util.Scanner;
 public class nprimenumbers {
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int count=0,c;
-		System.out.println("enter the limit");
-		int n=scan.nextInt();
-		for(int i=1;i<=n;i++){
-			c=0;
-			for(int j=1;j<=i;j++){
-				if(i%j==0)
-					c=c+1;
+		int count = 0;
+		int c;
+		int n = 15;
+		System.out.println("Prime Numbers till "+n+" is");
+		for (int i = 1; i <= n; i++) {
+			c = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0)
+					c = c + 1;
 			}
-		if(c==2)
-			System.out.println(++count+"prime numbers are"+i);
-	}
+			if (c == 2) {
+				++count;
+				System.out.println(i+" ");
+			}
+		}
+		System.out.println("Total:- "+count);
 	}
 }
+
+//input:- 10
+//output:-
+// 2
+// 3
+// 5
+// 7
