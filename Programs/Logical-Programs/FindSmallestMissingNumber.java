55b5801d83820e30c089fe10ee8bed577cea7b1e
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-01-20 00:07:17 +0530

    Update FindSmallestMissingNumber.java

--- Programs/Logical-Programs/FindSmallestMissingNumber.java
+++ Programs/Logical-Programs/FindSmallestMissingNumber.java
@@ -17,9 +17,15 @@ public class FindSmallestMissingNumber {
 		}
 	}
 
+	static int findMissingFromArrayUsingStream(int a[]) {
+
+		return IntStream.range(1, Integer.MAX_VALUE).filter(n -> Arrays.stream(a).noneMatch(num -> num == n)).findFirst().getAsInt();
+	}
+
 	public static void main(String[] args) {
 		int a[] = { 1, 2, 4 };
 		System.out.println("Smallest Missing Positive Integer is: " + findMissingFromArray(a));
+		System.out.println("Smallest Missing Positive Integer is: " + findMissingFromArrayUsingStream(a));
 	}
 }
 
