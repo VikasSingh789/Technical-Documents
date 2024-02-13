0c63980217c70225cb5250ff3d518cbd68aa9b2c
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-02-13 12:39:59 +0530

    Update PotHolesExample.java

--- Programs/UseCases/PotHolesExample.java
+++ Programs/UseCases/PotHolesExample.java
@@ -1,15 +1,15 @@
 public class PotHolesExample {
 	
 	public static int findPotHoles(String s) {
-		  int holes = 0;
-		  for (int i = 0; i < s.length(); i++) {
-		    if (s.charAt(i) == 'X') {
-		      holes++;
-		      i += 2;
-		    }
-		  }
-		  return holes;
+		int holes = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'X') {
+				holes++;
+				i += 2;
+			}
 		}
+		return holes;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println(findPotHoles("XXXX...X"));
